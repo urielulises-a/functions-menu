3bb4780a0d4b70f303fb03a578fbca7a9a5b3833
ElUriela <82078055+urielulises-a@users.noreply.github.com>
2021-07-26 22:58:31 -0500

    Add files via upload

---
+++ Problema 2.cpp
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+/*Problema 2: Un programa que realice 3 funciones y muestre un menu,
+numeros pares, perfectos y invertir un string*/
+
+using namespace std;
+	
+void es_par(){
+	system("cls");
+	
+	int num;
+	
+	printf("Ingresa 10 numeros enteros: \n");
+	
+	for (int i = 0; i<10; i++){
+		
+		scanf("%i", &num);
+		
+		if(num % 2 == 0){
+			
+			printf("El numero %i", num); printf(" es par. \n");
+		}
+		else {
+			
+			printf("El numero %i", num); printf(" no es par. \n");
+		}
+	}
+}
+void perfectos(){
+	system("cls");
+	
+	printf("Los numeros perfectos del uno al 100: \n");
+	int div, n, j = 1; 
+	
+	for (int i = 2; i<100; i++){
+		n = 0; //N se reinicia cada vez que se terminan de buscar los factores de un numero
+		while(j<100){
+			
+			div = i; 
+			j++;
+			if((div!=j) && (div % j == 0)){ //Mientras el numero sea distinto al divisor, va a ver si es un factor
+				n+= j;
+					
+				if(div == n && n!= 24){ //El programa muestra el 24 a pesar de no ser perfecto
+						
+					printf("%i ", n);
+				} 
+			}
+		}
+		j = 0; //Esta variable recorre los numeros hasta alcanzar a i
+	}
+}
+void invert(){
+	system("cls");
+	
+	string normal;
+	
+	std::string::reverse_iterator invert; 
+	
+	cout<<"Ingresa 4 numeros: "<<endl;
+	cin>>normal;
+	//Estas funciones recorren el vector de final a incio intercambiando posiciones
+	for (invert= normal.rbegin(); invert!= normal.rend(); invert++){
+		
+		cout<<*invert;
+	}	
+}
+
+int main(){
+	
+	char accion;
+	
+	do{
+		
+		printf("Menu de Opciones: \n\n"); printf("Conocer cual numero es par (a): \n");
+		printf("Numeros perfectos 1-100 (b): \n"); printf("Numeros en reversa (c): \n");
+		printf("Salir (s).\n");
+		
+		scanf("%c", &accion);
+		
+		switch (accion){
+			
+			case 'a': es_par();
+			case 'b': perfectos();
+			case 'c': invert();
+			case 's': system("cls"); printf("Salida exitosa. \n"); break;
+			default: printf("Opcion incorrecta.");
+		}
+		
+	}while (accion!= 's');
+}
+
+	
